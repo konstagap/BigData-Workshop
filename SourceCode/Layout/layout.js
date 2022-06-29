0a1d6085dbd4535e1227d5da9bf65c5680239026
Kostiantyn Agapov <konstagapov123@gmail.com>
2022-06-29 12:18:58 -0700

    Update layout.js

--- SourceCode/Layout/layout.js
+++ SourceCode/Layout/layout.js
@@ -1,6 +1,1 @@
 layout.jslayout.jslayout.jslayout.js
-updates2.jsupdates2.jsupdates2.jsupdates2.jsupdates2.js
-updates2.jsupdates2.jsupdates2.jsupdates2.jsupdates2.js
-updates2.jsupdates2.jsupdates2.jsupdates2.jsupdates2.js
-updates2.jsupdates2.jsupdates2.jsupdates2.jsupdates2.js
-updates2.jsupdates2.jsupdates2.jsupdates2.jsupdates2.js
